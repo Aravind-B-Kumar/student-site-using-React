7743d7b7bdbc3e16734c55b0f2302a62a19cba15
Aravind-B-Kumar <aravindkarullil@gmail.com>
2024-04-01 21:14:26 +0530

    student-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import AddStudent from './components/AddStudent';
+import SearchStudent from './components/SearchStudent';
+import StudentNavBar from './components/StudentNavBar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Students from './components/Students';
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        
+        <Route path='/' element={<Students/>}/>
+        <Route path='/add' element={<AddStudent/>}/>
+        <Route path='/search' element={<SearchStudent/>}/>
+        <Route path='/login' element={<Login/>}/>
+
+
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Students.jsx
@@ -0,0 +1,169 @@
+import React from 'react'
+import StudentNavBar from './StudentNavBar'
+
+const Students = () => {
+    return (
+
+        <div>
+            <StudentNavBar />
+
+
+            <div class="container">
+
+
+
+                <div class="row g-3">
+                    <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div class="row">
+                            <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <div class="card mb-3">
+                                    <div class="row g-0">
+                                        <div class="col-md-4">
+                                            <img src="https://www.socialsciencespace.com/wp-content/uploads/student-3500990_960_720_opt.jpg"
+                                                class="img-fluid rounded-start" alt="..." />
+                                        </div>
+                                        <div class="col-md-8">
+                                            <div class="card-body">
+                                                <h5 class="card-title">Abc</h5>
+                                                <p class="card-text">
+                                                    <p>Age:20</p>
+                                                    <p>Branch: Computer Science</p>
+                                                </p>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <div class="card mb-3">
+                                    <div class="row g-0">
+                                        <div class="col-md-4">
+                                            <img src="https://d2jyir0m79gs60.cloudfront.net/news/images/successful-college-student-lg.png"
+                                                class="img-fluid rounded-start" alt="..." />
+                                        </div>
+                                        <div class="col-md-8">
+                                            <div class="card-body">
+                                                <h5 class="card-title">Def</h5>
+                                                <p class="card-text">
+                                                    <p>Age:20</p>
+                                                    <p>Branch: Computer Science</p>
+                                                </p>
+
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <div class="card mb-3">
+                                    <div class="row g-0">
+                                        <div class="col-md-4">
+                                            <img src="https://eg5viqwfhsw.exactdn.com/uploads/2021/07/AdobeStock_356426229-scaled.jpeg?strip=all&lossy=1&ssl=1"
+                                                class="img-fluid rounded-start" alt="..." />
+                                        </div>
+                                        <div class="col-md-8">
+                                            <div class="card-body">
+                                                <h5 class="card-title">Ghi</h5>
+                                                <p class="card-text">
+                                                    <p>Age: 20</p>
+                                                    <p>Branch: Computer Science</p>
+                                                </p>
+
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <div class="card mb-3">
+                                    <div class="row g-0">
+                                        <div class="col-md-4">
+                                            <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg"
+                                                class="img-fluid rounded-start" alt="..." />
+                                        </div>
+                                        <div class="col-md-8">
+                                            <div class="card-body">
+                                                <h5 class="card-title">Jkl</h5>
+                                                <p class="card-text">
+                                                    <p>Age:20</p>
+                                                    <p>Branch: Computer Science</p>
+                                                </p>
+
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <div class="card mb-3">
+                                    <div class="row g-0">
+                                        <div class="col-md-4">
+                                            <img src="https://img.freepik.com/premium-photo/young-asian-indian-student-with-glasses-backpack-holds-book-shows-thumbs-up_928503-89.jpg"
+                                                class="img-fluid rounded-start" alt="..." />
+                                        </div>
+                                        <div class="col-md-8">
+                                            <div class="card-body">
+                                                <h5 class="card-title">Jkl</h5>
+                                                <p class="card-text">
+                                                    <p>Age:20</p>
+                                                    <p>Branch: Computer Science</p>
+                                                </p>
+
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                                <div class="card mb-3">
+                                    <div class="row g-0">
+                                        <div class="col-md-4">
+                                            <img src="https://img.freepik.com/free-photo/young-student-woman-wearing-denim-jacket-eyeglasses-holding-colorful-folders-showing-thumb-up-pink_176532-13861.jpg"
+                                                class="img-fluid rounded-start" alt="..." />
+                                        </div>
+                                        <div class="col-md-8">
+                                            <div class="card-body">
+                                                <h5 class="card-title">Jkl</h5>
+                                                <p class="card-text">
+                                                    <p>Age:20</p>
+                                                    <p>Branch: Computer Science</p>
+                                                </p>
+
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+    )
+}
+
+export default Students
